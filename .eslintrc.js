a49a8a6f4900422a609dfa7b23760d49a9c933fb
aaaaayako <ay14110mpda@gmail.com>
2019-12-23 12:16:14 +0900

    chore: fix linter and formatter config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,11 @@
-
 module.exports = {
-  "extends": [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-    "prettier",
-    "prettier/@typescript-eslint"
-  ],
-  "plugins": [
-    "@typescript-eslint"
-  ],
-  "env": { "node": true, "es6": true },
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "sourceType": "module",
-    "project": "./tsconfig.json"
+  extends: ['prettier'],
+  plugins: ['@typescript-eslint'],
+  env: { node: true, es6: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    project: './tsconfig.json',
   },
-  "rules": {
-  }
+  rules: {},
 }
